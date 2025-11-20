ebf558ac4d4a976be24d6fd6d847e24971db211e
kacper <kacper@192.168.1.7>
2025-11-20 15:22:38 +0100

    added animation

--- ai_terminal/main.py
+++ ai_terminal/main.py
@@ -1,7 +1,10 @@
 import sys
 import platform
+import threading
 import pyperclip
 from google import genai
+import itertools
+import time
 
 def readInput():
     text = ''
@@ -11,15 +14,21 @@ def readInput():
     return text
 
 def aiInit():
-
+    
+    done = False
     client = genai.Client()
     current_os = platform.system()
     if current_os == "Linux":
         os_info = platform.freedesktop_os_release()
         os_name = os_info.get('NAME')
         current_os = f"{current_os} {os_name}"
+   
+    text = readInput()
+    if text == '':
+        return 
 
-    aiCall = f"""You are a Command Line Interface generator.
+    aiCall = f"""
+    You are a Command Line Interface generator.
     Target Operating System: {current_os}
     Your task is to translate the user's natural language request into the correct command for this OS.
 
@@ -29,20 +38,31 @@ def aiInit():
     3. Do NOT include any explanations, notes, or extra text.
     4. If the user asks for a dangerous operation, output the command commented out with #.
     5. If the request is unrelated to terminal commands, ask the user to redefine his prompt and try again.
-    User Request: {readInput()}
+    User Request: {text}
     """ 
     
-    text = readInput()
-    if text == '':
-        return 
+    def animate():
+        for c in itertools.cycle(['|', '/', '-', '\\']):
+            if done:
+                break
+            sys.stdout.write('\rloading ' + c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+        #sys.stdout.write('\rDone!     ')
+
+    t = threading.Thread(target=animate)
+    t.start()
+
     final_prompt = aiCall.format(user_input=text)
 
-    response = client.models.generate_content(
+    aiModel = client.models.generate_content(
         model="gemini-2.5-flash", contents=final_prompt
     )
-    print(response.text)
+    response = aiModel.text
+    print(f"""\n{response}""")
+    done = True
     
-    if response.text == "Please redefine your prompt and try again.":
+    if response == "Please redefine your prompt and try again.":
         exit()
 
     while True:
@@ -50,12 +70,12 @@ def aiInit():
         c = input()
         
         if c=='y':
-            pyperclip.copy(response.text)
+            pyperclip.copy(response)
             break
         elif c=='n':
             break
         else:
-            print("Select [y/N] \n" )
+            print("Select [y/N] \n")
 
 
 if __name__ == '__main__':
