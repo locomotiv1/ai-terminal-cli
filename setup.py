b1f3af538b54f49034142f1f05c56d93520a8bde
locomotiv1 <140397959+locomotiv1@users.noreply.github.com>
2025-11-21 21:40:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ai-terminal',
+    name='ai-terminal-cli',
     version='0.1.0',
     description='A CLI tool to translate natural language into Linux commands.',
-    url="https://github.com/locomotiv1/ai-terminal",
+    url="https://github.com/locomotiv1/ai-terminal-cli",
     license='MIT',
     packages=find_packages(),
     install_requires=[
